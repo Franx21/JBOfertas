a02b88a3bf316c3623227581017c4bc4fe6e48a1
Franx21 <amvworld2015@gmail.com>
2024-02-26 17:42:17 -0700

    se crearon los controlladores

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the "web" middleware group. Make something great! 
 |
 */
 
@@ -25,6 +29,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentario.store');
+
+
+    Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
+    //Like de fotos
+    Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+    Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+    Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 });
 
 require __DIR__ . '/auth.php';
