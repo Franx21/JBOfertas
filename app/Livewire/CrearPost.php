617a8fe3ccd280db11569f14ef64cebe8e88a20d
Franx21 <amvworld2015@gmail.com>
2024-02-26 18:00:49 -0700

    se creo el componente de crear post

---
+++ app/Livewire/CrearPost.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CrearPost extends Component
+{
+    public function render()
+    {
+        return view('livewire.crear-post');
+    }
+}
